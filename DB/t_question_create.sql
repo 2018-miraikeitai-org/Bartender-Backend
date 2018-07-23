fe5ce9a9a0cc70b1b5ad4c89558da28f91966b4b
taigakimura <38418623+taigakimura@users.noreply.github.com>
2018-07-23 20:12:49 +0900

    create database

---
+++ DB/t_question_create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE question
+(ques_id	int	NOT NULL,
+# ques_name	varchar	NOT NULL,
+ques_contents	varchar NOT NULL,
+PRIMARY KEY(ques_id));
---
+++ DB/t_question_insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO question(ques_id, ques_contents) 
+VALUES (1, 'この中で好きなフルーツは？');
