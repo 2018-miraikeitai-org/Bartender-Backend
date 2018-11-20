9fb3f04b6519f41ea48b7cec8dd69c2caf545aa7
taigakimura <38418623+taigakimura@users.noreply.github.com>
2018-11-20 20:19:29 +0900

    SQLの修正

--- SQL/t_option_create.sql
+++ SQL/t_option_create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE option
-(option_id	serial	primary key,
-ques_id	int	REFERENCES question(ques_id),
-option_contents1	varchar(30) NOT NULL,
-option_contents2	varchar(30) NOT NULL,
-option_contents3	varchar(30),
-option_contents4	varchar(30));
+(option_id serial primary key,
+ques_id int REFERENCES question(ques_id),
+option_contents1 varchar(30) NOT NULL,
+option_contents2 varchar(30) NOT NULL,
+option_contents3 varchar(30),
+option_contents4 varchar(30));
